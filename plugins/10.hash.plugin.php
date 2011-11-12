602d585dbc857d2abc5a3eb8f992cd420dcd3e62
seth <xd.seth@gmail.com>
2011-11-12 18:23:37 -0100

    Hash plugin is now object oriented

--- plugins/10.hash.plugin.php
+++ plugins/10.hash.plugin.php
@@ -4,16 +4,24 @@
 		.hash <algorithm> <string>
 */
 
-function hashToChannel(&$irc,$msg,$channel,$matches,$who) 
-{
-    if ($matches[1] == 'help') {
-       	$irc->sayToChannel($who . ': Usage: .hash <algorithm> <string>', $channel);
-       	$irc->sayToChannel('Valid algos: ' . implode(' ', hash_algos()), $channel);
-    }else{
-      	$irc->sayToChannel($who . ': ' . hash($matches[1], $matches[2]), $channel);
-    }
+class hash_plugin{
+	
+	public function __construct(&$irc){
+		$algos = hash_algos();
+		$algos = array_map('preg_quote', $algos);
+		$irc->addHandler($this, 'hashToChannel', '/^\.hash (?:(help)|(' . implode('|', $algos) . ') (.*))/');
+	}
+	
+	/**
+	 * Outputs the hash of a string using the specified algorithm. Shows help if the algorithm is invalid
+	 */
+	public function hashToChannel(&$irc,$msg,$channel,$matches,$who) 
+	{
+		if ($matches[1] == 'help') {
+			$irc->sayToChannel($who . ': Usage: .hash <algorithm> <string>', $channel);
+			$irc->sayToChannel('Valid algos: ' . implode(' ', hash_algos()), $channel);
+		}else{
+			$irc->sayToChannel($who . ': ' . hash($matches[1], $matches[2]), $channel);
+		}
+	}
 }
-
-$algos = hash_algos();
-$algos = array_map('preg_quote', $algos);
-$this->handlers['*']['hashToChannel'] = '/^\.hash (?:(help)|(' . implode('|', $algos) . ') (.*))/';
