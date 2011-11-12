f40f7de99fb8c8be994ce26670c22fee25f7a2fd
seth <xd.seth@gmail.com>
2011-11-12 19:31:47 -0100

    only an admin can use .config

--- plugins/10.manageconfig.plugin.php
+++ plugins/10.manageconfig.plugin.php
@@ -17,36 +17,38 @@ class manageconfig_plugin{
 	public function manageConfig(&$irc,$msg,$channel,$matches,$who) 
 	{
 		
-		$properties = $irc->database->get(null, 'manage-config');
-			
-		//List all the properties
-		if (substr($msg, 0, 12) == '.config list') {
-			foreach ($properties as $property) {
-				foreach ($property as $name => $value) {
-					$irc->sayToChannel($name.' => '.$value, $channel);
+		if ($irc->userLevels->getLevel($who) >= USER_LEVEL_OWNER) {
+			$properties = $irc->database->get(null, 'manage-config');
+				
+			//List all the properties
+			if (substr($msg, 0, 12) == '.config list') {
+				foreach ($properties as $property) {
+					foreach ($property as $name => $value) {
+						$irc->sayToChannel($name.' => '.$value, $channel);
+					}
 				}
-			}
-			
-		//Show one property value
-		}elseif (preg_match('@^\.config get (\S+)@', $msg, $matches)) {
-			foreach ($properties as $property) {
-				if (isset($property[$matches[1]])) {
-					$irc->sayToChannel($matches[1].' => '.$property[$matches[1]], $channel);
+				
+			//Show one property value
+			}elseif (preg_match('@^\.config get (\S+)@', $msg, $matches)) {
+				foreach ($properties as $property) {
+					if (isset($property[$matches[1]])) {
+						$irc->sayToChannel($matches[1].' => '.$property[$matches[1]], $channel);
+					}
 				}
-			}
-			
-		//Change an existing property or add a new one
-		}elseif (preg_match('@^\.config set (\S+) (.+)@', $msg, $matches)) {
-			foreach ($properties as $property) {
-				if (isset($property[$matches[1]])) {
-					$irc->database->change($matches[1], $matches[2],  null, 'manage-config');
-					break 1;
+				
+			//Change an existing property or add a new one
+			}elseif (preg_match('@^\.config set (\S+) (.+)@', $msg, $matches)) {
+				foreach ($properties as $property) {
+					if (isset($property[$matches[1]])) {
+						$irc->database->change($matches[1], $matches[2],  null, 'manage-config');
+						break 1;
+					}
 				}
+				if (!isset($property[$matches[1]])) {
+					$irc->database->insert($matches[1], $matches[2], 'manage-config');
+				}
+				$irc->$property=$value;
 			}
-			if (!isset($property[$matches[1]])) {
-				$irc->database->insert($matches[1], $matches[2], 'manage-config');
-			}
-			$irc->$property=$value;
 		}
 	}
 	
