22a366c53da0b3699d464f91b70f9cc85c9f26f5
seth <xd.seth@gmail.com>
2011-11-12 17:15:35 -0100

    Updated say plugin to work with the new plugin system

--- plugins/10.say.plugin.php
+++ plugins/10.say.plugin.php
@@ -4,14 +4,17 @@
 		.say <#channel> <something>
 		.say <somebody> <something>
 */
+class say_plugin{
+	public function __construct(&$irc){	
+		$irc->addHandler($this, 'repeatOnChannel', '/^\.say (.*?) (.*)/s');	
+	}
 
-function repeatOnChannel(&$irc,$msg,$channel,$matches,$who) 
-{
-	if (($irc->user_levels->getLevel($who) >= USER_LEVEL_OWNER) or
-		(($irc->user_levels->getLevel($who) >= USER_LEVEL_ADMIN) and (strpos(strtolower($matches[1]), 'serv') === false))) {
-			//So, the user is an admin not talking to a service or the user is a owner
-			$irc->sayToChannel($matches[2],$matches[1]);
+	public function repeatOnChannel(&$irc,$msg,$channel,$matches,$who) 
+	{
+		if (($irc->user_levels->getLevel($who) >= USER_LEVEL_OWNER) or
+			(($irc->user_levels->getLevel($who) >= USER_LEVEL_ADMIN) and (strpos(strtolower($matches[1]), 'serv') === false))) {
+				//So, the user is an admin not talking to a service or the user is a owner
+				$irc->sayToChannel($matches[2],$matches[1]);
+		}
 	}
 }
-
-$this->handlers['*']['repeatOnChannel'] = '/^\.say (.*?) (.*)/s';
