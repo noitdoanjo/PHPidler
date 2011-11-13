dd3b80cb0e9695803576518026efca516e506dc6
seth <xd.seth@gmail.com>
2011-11-13 21:51:47 -0100

    added flood plugin

---
+++ plugins/10.flood.plugin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+	Commands:
+		.flood 10 who what
+		.floodctcp 10 who what
+*/
+class flood_plugin{
+
+	public function __construct(&$irc){	
+		$irc->addHandler($this, 'flood', '/^\.(flood|floodctcp) (\d+) (\S+?) (.+)/');
+	}
+
+	public function flood(&$irc,$msg,$channel,$matches,$who)
+	{
+		if (($irc->userLevels->getLevel($who) >= USER_LEVEL_OWNER) or
+			(($irc->userLevels->getLevel($who) >= USER_LEVEL_ADMIN) and (strpos(strtolower($matches[3]), 'serv') === false))) {
+				//So, the user is an admin not talking to a service or the user is a owner
+                if ($matches[1] == 'floodctcp') {
+                    $matches[4] = chr(1) . $matches[4] . chr(1);
+                }
+                for ($i = 0; $i < $matches[2]; $i++){
+    				$irc->sayToChannel($matches[4],$matches[3], ($matches[1] == 'floodctcp'));
+                    usleep(500000);
+                }            
+		}
+	}
+}
