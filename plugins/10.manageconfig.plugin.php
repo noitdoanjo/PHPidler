302811701724d296be0671731ada63b6293a0fdf
seth-- <xd.seth@gmail.com>
2012-04-17 23:25:47 +0000

    started to rewrite manageconfig

--- plugins/10.manageconfig.plugin.php
+++ plugins/10.manageconfig.plugin.php
@@ -1,63 +1,42 @@
 <?php
 /*
 	Commands:
-		.config list
 		.config get <property>
 		.config set <property> <value>
+		.config list
+		.config load <file>
+		.config reload
 */
 
-//TODO: mostrar en "list" los parametros configurados en config,php
 class manageconfig_plugin{
 
-	public function __construct(&$irc){
-		$this->loadConfig($irc);
-		$irc->addActionHandler($this, 'manageConfig', '/^\.config (set (\S+) (.+)|get (.+)|list)/');
-	}
-	
-	public function manageConfig(&$irc,$msg,$channel,$matches,$who) 
+	public function __construct(&$irc)
 	{
-		
-		if ($irc->userLevels->getLevel($who) >= USER_LEVEL_OWNER) {
-			$properties = $irc->database->get(null, 'manage-config');
-				
-			//List all the properties
-			if (substr($msg, 0, 12) == '.config list') {
-				foreach ($properties as $property) {
-					foreach ($property as $name => $value) {
-						$irc->sayToChannel($name.' => '.$value, $channel);
-					}
-				}
-				
-			//Show one property value
-			}elseif (preg_match('@^\.config get (\S+)@', $msg, $matches)) {
-				foreach ($properties as $property) {
-					if (isset($property[$matches[1]])) {
-						$irc->sayToChannel($matches[1].' => '.$property[$matches[1]], $channel);
-					}
-				}
-				
-			//Change an existing property or add a new one
-			}elseif (preg_match('@^\.config set (\S+) (.+)@', $msg, $matches)) {
-				foreach ($properties as $property) {
-					if (isset($property[$matches[1]])) {
-						$irc->database->change($matches[1], $matches[2],  null, 'manage-config');
-						break 1;
-					}
-				}
-				if (!isset($property[$matches[1]])) {
-					$irc->database->insert($matches[1], $matches[2], 'manage-config');
-				}
-				$irc->$matches[1]=$matches[2];
-			}
-		}
+		$irc->addActionHandler($this, 'get', '/^\.config get (.+)/');
+		$irc->addActionHandler($this, 'set', '/^\.config set (\S+) (.+)/');
+		$irc->addActionHandler($this, 'showList', '/^\.config( list)?/');
+		$irc->addActionHandler($this, 'load', '/^\.config load (.+)/');
+		$irc->addActionHandler($this, 'reload', '/^\.config reload/');
 	}
 	
-	private function loadConfig(&$irc){
-		$properties = $irc->database->get(null, 'manage-config');
-		foreach ($properties as $property) {
-			foreach ($property	as $name => $value) {
-				$irc->$name=$value;
-			}
-		}
+	
+	private function get(&$irc,$msg,$channel,$matches,$who)
+	{
+	}
+	
+	public function set(&$irc,$msg,$channel,$matches,$who) 
+	{
+	}
+	
+	private function showList(&$irc,$msg,$channel,$matches,$who)
+	{
+	}
+	
+	private function reload(&$irc,$msg,$channel,$matches,$who)
+	{
+	}
+	
+	private function load(&$irc,$msg,$channel,$matches,$who)
+	{
 	}
 }
